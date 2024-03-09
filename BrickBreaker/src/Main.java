d4862d3348ff36dbe426af97e3ac3b9ed79fc153
WorldOfBerk <baris.berk.sengl@hotmail.com>
2024-03-09 22:43:34 +0300

    setting up JFrame

--- BrickBreaker/src/Main.java
+++ BrickBreaker/src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Creating JFrame
+        JFrame frame = new JFrame("Brick Breaker?");
+
+        GamePlay gamePlay = new GamePlay();
+
+        frame.setBounds(10, 10, 700, 600);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
     }
 }

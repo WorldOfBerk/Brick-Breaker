b325cb795e657e4468ee9d5bc916c5a59aa8bb36
WorldOfBerk <baris.berk.sengl@hotmail.com>
2024-03-09 23:03:23 +0300

    Add initial game setup and map generation

--- BrickBreaker/src/GamePlay.java
+++ BrickBreaker/src/GamePlay.java
@@ -5,9 +5,33 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks = 21;
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballXdirection = -1;
+    private int ballYdirection = -2;
+
+    private MapGenerator map;
+    public GamePlay(){
+        map = new MapGenerator(3, 7);
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
 
     @Override
